822f5b9fbc1bd2730399834586ccd3605ccf8459
lu97chi <lu97is@gmail.com>
2018-02-01 16:04:40 -0700

    conexion

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const EXPRESS = require('express');
 const APP = EXPRESS();
 const MONGOOSE = require('mongoose');
-MONGOOSE.connect('mongodb://lu97is:eagle1997@ds117148.mlab.com:17148/nodetest1997', (err)=>{
+MONGOOSE.connect('mongodb://lu97is:eagle1997@ds117148.mlab.com:17148/nodetest1997', function (err) {
     if (err) {
-        console.log('error' + err)
+        console.log('error')
     } else {
-        console.log('conectado')        
+        console.log('conectado')
     }
 })
-APP.get('/',(req,res)=>{
+APP.get('/', (req, res) => {
     res.send('hola mundo');
 });
 
-APP.listen(8080,()=>{
+APP.listen(8080, () => {
     console.log('servidor en puerto 8081')
 })
